d05188825e8d6dd3c621b0f03e3dade0f8da0bdc
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-08-19 21:41:50 +0300

    - Created country class to be used for project instantiation. - Update nuget packages.

---
+++ RestStandarts.API/Models/Country.cs
@@ -0,0 +1,12 @@
+﻿namespace RestStandarts.API.Models
+{
+    public class Country
+    {
+        public Guid Id { get; set; }
+        public string NumericCode { get; set; }
+        public string AlphaCode2 { get; set; }
+        public string AlphaCode3 { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
