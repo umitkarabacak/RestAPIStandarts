593380ecc84bd8ca7998f343da27b4a3f45f1e8a
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-08-19 21:39:32 +0300

    - project dependencies moved to DependencyInjection.cs for tracking - An example of global usage has been created in DependencyInjection.cs

---
+++ RestStandarts.API/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿global using RestStandarts.API;
+
+namespace RestStandarts.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
